393193710057f81ba519bc6170d2fcf46c874262
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-06-18 14:31:08 -0400

    duplicate the tank drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,41 +4,54 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_robotDrive;
+  private DifferentialDrive m_frontRobotDrive;
+  private DifferentialDrive m_backRobotDrive;
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  // TODO Update device IDs either on the robot or here
+  private final CANSparkMax m_frontLeftMotor = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax m_frontRightMotor = new CANSparkMax(1, MotorType.kBrushless);
+  private final CANSparkMax m_backLeftMotor = new CANSparkMax(2, MotorType.kBrushless);
+  private final CANSparkMax m_backRightMotor = new CANSparkMax(3, MotorType.kBrushless);
 
   @Override
   public void robotInit() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    SendableRegistry.addChild(m_frontRobotDrive, m_frontLeftMotor);
+    SendableRegistry.addChild(m_frontRobotDrive, m_frontRightMotor);
+
+    SendableRegistry.addChild(m_backRobotDrive, m_backLeftMotor);
+    SendableRegistry.addChild(m_backRobotDrive, m_backRightMotor);
 
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    m_frontRightMotor.setInverted(true);
+    m_backRightMotor.setInverted(true);
 
-    m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
+    m_frontRobotDrive = new DifferentialDrive(m_frontLeftMotor::set, m_frontRightMotor::set);
+    m_backRobotDrive = new DifferentialDrive(m_backLeftMotor::set, m_backRightMotor::set);
+
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    m_frontRobotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    m_backRobotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
   }
 }
