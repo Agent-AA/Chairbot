750bb64f4d14412702bd806302332d0a0532b63f
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-06-25 15:11:45 -0400

    chairbot works now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,26 +4,39 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive =
-      new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
+  private final WPI_TalonSRX m_frontLeftMotor = new WPI_TalonSRX(3);
+  private final WPI_VictorSPX m_frontRightMotor = new WPI_VictorSPX(5);
+  private final WPI_TalonSRX m_backLeftMotor = new WPI_TalonSRX(2);
+  private final WPI_VictorSPX m_backRightMotor = new WPI_VictorSPX(4);
+  
+  private final DifferentialDrive m_robotFrontDrive =
+      new DifferentialDrive(m_frontLeftMotor::set, m_frontRightMotor::set);
+  
+  private final DifferentialDrive m_robotBackDrive =
+      new DifferentialDrive(m_backLeftMotor::set, m_backRightMotor::set);
+
   private final Joystick m_stick = new Joystick(0);
 
   public Robot() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    SendableRegistry.addChild(m_robotFrontDrive, m_frontLeftMotor);
+    SendableRegistry.addChild(m_robotFrontDrive, m_frontRightMotor);
+
+    SendableRegistry.addChild(m_robotBackDrive, m_backLeftMotor);
+    SendableRegistry.addChild(m_robotBackDrive, m_backRightMotor);
   }
 
   @Override
@@ -31,7 +44,8 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    m_frontRightMotor.setInverted(true);
+    m_backRightMotor.setInverted(true);
   }
 
   @Override
@@ -39,6 +53,7 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    m_robotFrontDrive.arcadeDrive(-m_stick.getY(), -m_stick.getZ());
+    m_robotBackDrive.arcadeDrive(-m_stick.getY(), -m_stick.getZ());
   }
 }
